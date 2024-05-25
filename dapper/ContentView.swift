13538eca62b63a7d6c8b223472886310c3d13fa1
Salvatore Darmetta <sdarmetta@gmail.com>
2024-05-25 16:23:48 -0400

    Initial Commit

---
+++ dapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dapper
+//
+//  Created by Salvatore D'Armetta on 5/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dapper/dapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  dapperApp.swift
+//  dapper
+//
+//  Created by Salvatore D'Armetta on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct dapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
